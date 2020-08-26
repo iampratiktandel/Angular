0a80aa5806f12860723c1e0b500a2413e1bfdc90
iampratiktandel <iampratiktandel@gmail.com>
2020-08-26 18:18:12 +0530

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from "@angular/http";
 
 import { baseURL } from "./shared/baseurl";
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -51,7 +52,8 @@ import { baseURL } from "./shared/baseurl";
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
